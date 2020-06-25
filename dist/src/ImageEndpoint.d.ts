c79263a8d117538d844dde2f601c60b51ffeeae4
Allvaa <alvamailindo@gmail.com>
2020-06-26 01:01:11 +0700

    recompile and regen docs

--- dist/src/ImageEndpoint.d.ts
+++ dist/src/ImageEndpoint.d.ts
@@ -9,7 +9,7 @@ declare class ImageEndpoint {
     client: NekoBot;
     constructor(client: NekoBot);
     /**
-     * @param {ImageEndpointType} type - The type of image to get. Current types: hass, hmidriff, pgif, 4k, hentai, holo, hneko, neko, hkitsune, kemonomimi, anal, hanal, gonewild, kanna, ass, pussy, thigh, hthigh, gah, coffee, food, cosplay.
+     * @param {ImageEndpointType} type - The type of image to get. Current types: hass, hmidriff, pgif, 4k, hentai, holo, hneko, neko, hkitsune, kemonomimi, anal, hanal, gonewild, kanna, ass, pussy, thigh, hthigh, gah, coffee, food, cosplay, paizuri, tentacle.
      * @returns {(Promise<String | void>)} Image URL
      */
     getImage(type: ImageEndpointType): Promise<String | void>;
